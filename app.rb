0ca6682bbc42ec22cb41ad2272b7de7410abafec
lemma-85 <rmamuye@chicagobooth.edu>
2025-04-14 14:34:40 +0000

    changes

--- app.rb
+++ app.rb
@@ -21,11 +21,33 @@ get("/rock") do
 erb(:zebra)
 end
 
-get("/paper") do
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
 
+  @comp_move = moves.sample
+
+  if @comp_move == "scissors"
+    @outcome = "tied"
+  elsif @comp_move == "rock"
+    @outcome = "lost"
+  else 
+    @outcome = "won"
+  end 
   erb(:giraffe)
 end
 
-get("/scissors") do
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "paper"
+    @outcome = "tied"
+  elsif @comp_move == "scissors"
+    @outcome = "lost"
+  else 
+    @outcome = "won"
+  end 
+
   erb(:lion)
 end
